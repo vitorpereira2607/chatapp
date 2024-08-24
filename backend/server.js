0902eae88cdcd2978c8737ac075b330e6bd3c5f4
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-08-24 19:51:27 +0100

    add port variable to listen

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,4 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send("Hello World"))
 
-app.listen(5000, () => console.log('server running: http://localhost:5000'))
+app.listen(PORT, () => console.log('server running: http://localhost:5000'))
