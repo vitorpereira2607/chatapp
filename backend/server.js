beeb16cf25b4660f510884d8fe4f5d3c2346f3f9
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-08-24 19:14:21 +0100

    express config and vite react installation

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+const PORT = process.env.port || 5000;
+
+app.get('/', (req, res) => res.send("Hello World"))
+
+app.listen(5000, () => console.log('server running: http://localhost:5000'))
