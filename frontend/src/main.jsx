0222e04b6c22bc71ffc59f2d6f5d3fac94450399
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-09-10 19:03:05 +0100

    Remove Toaster from Home Page to main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContex.jsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
+        <Toaster />
         <App />
       </AuthContextProvider>
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import MessageContainer from "../components/messages/MessageContainer"
 import Sidebar from "../components/sidebar/Sidebar"
-import { Toaster } from 'react-hot-toast';
 function Home() {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <Toaster />
         <Sidebar />
         <MessageContainer />
     </div>
