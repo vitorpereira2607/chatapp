21a7687b68378ba5fd6b03f06376b884ff8411af
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-09-10 23:22:51 +0100

    load .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false, 
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,
+        }
       }
-    }
-  },
+    },
+  }
 })
