05588a06edb81c3ea44bea0a073c21b2b31bcc2c
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-09-10 14:16:06 +0100

    create sendMessage hook

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,15 +1,26 @@
 import MessageInput from './MessageInput'
 import Messages from './Messages'
 import NoChatSelected from './NoChatSelected'
+import useConversation from '../../zustand/useConversation'
+import { useEffect } from 'react';
 
 function MessageContainer() {
-  const chatSelected = true;
+  const {selectedConversation, setSelectedConversation} = useConversation();
+
+  useEffect(() => {
+    
+    // cleanup selectedConversation
+    return () => {
+      setSelectedConversation(null)
+    }
+  }, [])
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {chatSelected ? <NoChatSelected /> : (
+      {!selectedConversation ? <NoChatSelected /> : (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span> <span className="text-gray-100">John Doe</span>
+            <span className="label-text">To:</span> <span className="text-gray-100">{selectedConversation.fullName}</span>
           </div>
           <Messages />
           <MessageInput />
